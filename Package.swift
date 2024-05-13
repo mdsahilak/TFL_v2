e9e5d3a39ef63ce479c96e3e4d28e033d1396c07
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-05-13 01:23:52 +0100

    Add advanced standard lib packages - swift-collections and swift-algorithms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "TFL_v2",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "1.2.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
