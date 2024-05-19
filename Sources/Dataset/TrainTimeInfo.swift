34b2b11186189d07ffc54723bbe1e53810ddf36b
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-05-19 18:54:01 +0100

    typo fixes

--- Sources/Dataset/TrainTimeInfo.swift
+++ Sources/Dataset/TrainTimeInfo.swift
@@ -9,9 +9,9 @@ import Foundation
 
 struct TrainTimeInfo: Decodable {
     var line: String { line_.capitalized.trimmingCharacters(in: .whitespaces) }
-    var direction: String { direction_.capitalized }
-    var departure: String { departure_.capitalized }
-    var destination: String { destination_.capitalized }
+    var direction: String { direction_.capitalized.trimmingCharacters(in: .whitespaces) }
+    var departure: String { departure_.capitalized.trimmingCharacters(in: .whitespaces) }
+    var destination: String { destination_.capitalized.trimmingCharacters(in: .whitespaces) }
     var time: Double { time_ }
     
     private var line_: String
